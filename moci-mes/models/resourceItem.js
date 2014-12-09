c5cd7c2336e079b88930861d96eb55c1384968b5
jicui <jicui@aliyun.com>
2014-12-09 13:19:51 +0800

    init poc branch

--- moci-mes/models/resourceItem.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(resource, items) {
-    this.resource = resource;
-    this.items = items;
-}
-
-
--- moci-mrs/src/main/java/com/jcworks/happymock/command/ICommand.java
+++
@@ -1,9 +0,0 @@
-package com.jcworks.happymock.command;
-
-/**
- * User: jicui
- * Date: 14-10-27
- */
-public interface ICommand {
-   public void run();
-}
