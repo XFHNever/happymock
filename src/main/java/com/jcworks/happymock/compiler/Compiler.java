e4b84205eea0db99049fc1d8d8188801bf1fc1e1
jicui <jicui@aliyun.com>
2014-12-10 19:17:04 +0800

    init set up gradle env

---
+++ src/main/java/com/jcworks/happymock/compiler/Compiler.java
@@ -0,0 +1,8 @@
+package com.jcworks.happymock.compiler;
+
+/**
+ * User: jicui
+ * Date: 14-12-10
+ */
+public interface Compiler {
+}
---
+++ src/test/java/CompilerTest.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+/**
+ * User: jicui
+ * Date: 14-12-10
+ */
+@Test
+public class CompilerTest {
+}
